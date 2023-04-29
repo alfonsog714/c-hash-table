d7ec3cf3f0e435d96f316fc7d752a8bb043c75ab
Alfonso Garcia <alfonso.garcia2324@gmail.com>
2023-04-29 10:50:50 -0700

    get_hash() implemented & fleshing out more declarations in hash_table header

--- src/hash_table.c
+++ src/hash_table.c
@@ -5,6 +5,9 @@
 
 #include "hash_table.h"
 
+#define HT_PRIME_1 131
+#define HT_PRIME_2 149
+
 static ht_item *ht_new_item(const char *k, const char *v) {
   ht_item *i = malloc(sizeof(ht_item));
   i->key = strdup(k);
@@ -49,4 +52,10 @@ static int ht_hash(const char *s, const int a, const int m) {
   }
 
   return (int)hash;
+}
+
+static int get_hash(const char *s, const int num_buckets, const int attempt) {
+  const int hash_a = ht_hash(s, HT_PRIME_1, num_buckets);
+  const int hash_b = ht_hash(s, HT_PRIME_1, num_buckets);
+  return (hash_a + (attempt * (hash_b + 1))) % num_buckets;
 }
